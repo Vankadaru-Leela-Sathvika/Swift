0901c4f132845972e2bc460a494151e487dd2fe7
Leela Sathvika Vankadaru <69385943+Vankadaru-Leela-Sathvika@users.noreply.github.com>
2022-04-28 11:00:12 +0530

    basic

--- basics/basic.swift
+++ basics/basic.swift
@@ -1,7 +1,29 @@
 //Constants and Variables
 let uniqueId="454554574545405"
+let firstName:String //type Annotation
+firstName="Leela Sathvika" //Intializing Constant
+/*
+firstName="Sathvika"
+Error: note: change 'let' to 'var' to make it mutable
 let firstName
-firstName="Leela Sathvika"
+^~~
+var
+*/
 var surName:String //type annotation
 surName="Vankadaru"
+var age:Int,cgpa:Double
+var tenth,twelth:Double 
+print("Type of age: \(type(of:age)), cgpa: \(type(of:cgpa)), tenth: \(type(of:tenth)), twelth: \(type(of:twelth))")
+age=20
+cgpa=9.59
+tenth=98
+twelth=93
+print("Age: \(age), CGPA: \(cgpa), 10th: \(tenth), twelth: \(twelth)")
+var cgpaRounded=Int(cgpa)
+print("cgpaRounded: \(cgpaRounded)")
 
+//Type Alias
+typealias distance = UInt8
+var toZoho:distance=40
+print("Distance to Zoho:\(toZoho)km")
+print("Max: \(distance.max), Min: \(distance.min)")
