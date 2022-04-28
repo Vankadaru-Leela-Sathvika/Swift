f2e07e1244ed1114aafb08d821e82d722c7e285b
Leela Sathvika Vankadaru <69385943+Vankadaru-Leela-Sathvika@users.noreply.github.com>
2022-04-28 18:04:14 +0530

    enum under build

--- Custom Types/Enumeration.swift
+++ Custom Types/Enumeration.swift
@@ -1,11 +1,10 @@
-indirect enum expression{
-    case operand(Int)
-    case add(expression,expression)
-    case subtract(expression,expression)
-    case multiply(expression,expression)
-    case divide(expression,expression)
-
-    func evaluate(_ exp:expression){
-        
-    }
+enum Shape{
+    case triangle(Int,Int,Int)
+    case square(Int,Int,Int,Int)
+    case rectangle(Int,Int,Int,Int)
+    case pentagon(Int,Int,Int,Int,Int)
 }
+var triangle=Shape.triangle(10,20,30)
+var square=Shape.square(10,10,10,10)
+
+var shapes:[Shape]=[triangle,square]
