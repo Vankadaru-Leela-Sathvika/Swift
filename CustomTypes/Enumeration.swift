2504e6ea3c76f4977801fb8e29dcf7fc601cff23
Leela Sathvika Vankadaru <69385943+Vankadaru-Leela-Sathvika@users.noreply.github.com>
2022-04-29 15:20:26 +0530

    change file name

---
+++ CustomTypes/Enumeration.swift
@@ -0,0 +1,72 @@
+enum Shape{
+    case square(Int)
+    case rectangle(Int,Int)
+    case triangle(Double,Double)
+}
+var square=Shape.square(10)
+var rectangle=Shape.rectangle(10,20)
+var triangle=Shape.triangle(10,20)
+var shapes=[square,rectangle,triangle]
+
+func calculateArea(shape:Shape)->Void{
+    switch shape{
+        case .square(let side):
+            print("This is a square! The area is \(side*side)")
+        case let .rectangle(length,breadth):
+            print("This is a Rectangle! The area is \(length*breadth)")
+        case let .triangle(base,height):
+            print("This is a Triangle! The area is \(0.5*base*height)")
+    }
+}
+for shape in shapes{
+    calculateArea(shape:shape)
+}
+// enum Beverage: CaseIterable { - incompatible needs swift 4.2
+//     case coffee, tea, juice
+// }
+// let numberOfChoices = Beverage.allCases.count
+// print("\(numberOfChoices) beverages available")
+
+enum Season:String{
+    case spring,summer,winter,autumn
+}
+print(Season.spring.rawValue)
+
+//rawValues
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+let possiblePlanet = Planet(rawValue: 7)
+let positionToFind = 11
+if let somePlanet = Planet(rawValue: positionToFind) {
+    switch somePlanet {
+    case .earth:
+        print("Mostly harmless")
+    default:
+        print("Not a safe place for humans")
+    }
+} else {
+    print("There isn't a planet at position \(positionToFind)")
+}
+
+//recursive enumerations use indirect keyword
+indirect enum LinkedListItem{
+    case endPoint(value: Int)
+    case linkNode(value: Int, next: LinkedListItem)
+}
+let third = LinkedListItem.endPoint(value: 3)
+let second = LinkedListItem.linkNode(value: 2, next: third)
+let first = LinkedListItem.linkNode(value: 1, next: second)
+
+var currentNode = first
+
+listLoop: while true {
+    switch currentNode {
+    case .endPoint(let value):
+        print(value)
+        break listLoop
+    case .linkNode(let value, let next):
+        print(value)
+        currentNode = next
+    }
+}
